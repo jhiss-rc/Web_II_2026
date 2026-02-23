2d65c7db21f5e2d97d1465bfe2a86224a2ad2ac4
jhiss-rc <jhissel.rodriguez.cont@gmail.com>
2026-02-23 08:31:36 -0400

    Ejercicios del 1 al 10

--- Unidad_1/repasoJS/9-operacionesdeComparacion.js
+++ Unidad_1/repasoJS/9-operacionesdeComparacion.js
@@ -1,6 +1,28 @@
 // === valor absoluto que si o si debe cumplirse (se debe usar)
-const valorPasaje = 100;
-if(valorPasaje == 100)
+const valorPasaje=1000
+if(valorPasaje == 1000)
 {
-    console.log(`el pasaje es correcto`)
+    console.log(`el pasaje es correcto`);
+}
+const paisDestino="Ecuador";
+const paisesDisponibles = ["Bolvia","ecuador","brasil","vanezuela","italia","francia"];
+
+let edadPasaje=17;
+let acompaniado=true;
+
+console.log(`pasajes para ${paisesDisponibles}`);
+if((paisesDisponibles.indexOf(paisDestino)> -1 && (edadPasaje >=18 || acompaniado)))
+{
+    console.log("pasaje disponible para venta");
+}else
+{
+    console.log("no se puede vender el pasaje");
+}
+
+if(!(paisesDisponibles.indexOf(paisDestino)> -1 && (edadPasaje >=18 || acompaniado)))
+{
+    console.log("pasaje disponible para venta");
+}else
+{
+    console.log("no se puede vender el pasaje");
 }
