1354396e1bf49cce678dd325c47c85f61249939d
jhiss-rc <jhissel.rodriguez.cont@gmail.com>
2026-03-16 10:15:12 -0400

    DOM avanzado

---
+++ Unidad_1/DOM AVANZADO/script.js
@@ -0,0 +1,9 @@
+import Form from "./components/formulario.js"
+import Form from "./components/tabla.js"
+import Form from "./components/cards.js"
+(()=>{
+    Form.setDatos((rask)=>{
+        tabla.addTask(task);
+        cards.update();
+    })
+})();
